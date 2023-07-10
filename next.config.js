fa57d8a96e03f91c8ca25a708929152c4078d82c
Nainish Rai <nainishraisjp@gmail.com>
2023-07-10 15:53:15 +0530

    vercel conf

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     domains: ["scontent.cdninstagram.com", "static.cdninstagram.com"],
     remotePatterns: [
       {
